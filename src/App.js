b9a1bc27ee3f6fb249d1f4c08f73ee30da56188c
VIll999 <wu1908@purdue.edu>
2024-10-30 03:31:08 -0400

    minor fix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import AccountSettings from './Components/HomePage/AccountSettings';
 import CreateProductEntry from './Components/HomePage/CreateProductEntry';
 import ProductEntry from './Components/ProductEntry/ProductEntry';
 import EditProduct from './Components/ProductEntry/EditProductEntry';
-import productListing from './Components/HomePage/productListing';
+import ProductListing from './Components/HomePage/productListing';
 
 function App() {
   return (
@@ -26,7 +26,7 @@ function App() {
         <Route path="/createProductEntry" element={<CreateProductEntry />} />
         <Route path="/product/:productId" element={<ProductEntry />} /> {/* Dynamic route for product details */}
         <Route path="/editproduct" element={<EditProduct />} />
-        <Route path="/productListing" element={<productListing />} />
+        <Route path="/productListing" element={<ProductListing />} />
       </Routes>
     </Router>
   );

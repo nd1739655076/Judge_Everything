85e235ae478210f7f404c213840851ebe8a841c0
Yuanfei Song <nd1739655076@gmail.com>
2024-11-20 22:03:25 -0500

    Fix redirection of regular admin home. It is /admin/regularHome instead of admin/home

--- src/AdminClient/Login/AdminLogin.jsx
+++ src/AdminClient/Login/AdminLogin.jsx
@@ -57,7 +57,7 @@ const AdminLogin = () => {
             }, 500);
           } else {
             setTimeout(() => {
-              navigate("/admin/home");
+              navigate("/admin/regularHome");
             }, 500);
           }
           

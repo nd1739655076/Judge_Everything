e47eeb43e0144c3edec8968e4af3f087baf9955c
DlawD <111907705+DlawD@users.noreply.github.com>
2024-11-20 20:54:59 -0500

    Delete generated/javascript/default directory

--- generated/javascript/default/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'CS 307 Project',
-  location: 'us-central1'
-};
-
--- generated/javascript/default/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'CS 307 Project',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
